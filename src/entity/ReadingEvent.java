289e019fe0e557ec845169c3e6d90f13e603defe
Maksim Tsoi <maksim.tsoi@mail.utoronto.ca>
2023-10-23 12:47:51 -0400

    Implemented base functions for ReadingEvent.

--- src/entity/ReadingEvent.java
+++ src/entity/ReadingEvent.java
@@ -1,4 +1,42 @@
 package entity;
 
+import java.util.Optional;
+
 public class ReadingEvent extends Event{
+
+    private Resource resource;
+    private boolean required;
+    private Optional<int[]> pages;
+
+
+    public ReadingEvent(Resource resource) {
+        this.resource = resource;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    public Optional<int[]> getPages() {
+        return pages;
+    }
+
+    public void setPages(Optional<int[]> pages) {
+        this.pages = pages;
+    }
+    public void removePages(){
+        //TODO
+    }
 }
