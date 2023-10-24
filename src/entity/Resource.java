b7c20c2fe56e3cde132115e99e201f4d7c5cdcb5
ksaykili <106082029+ksaykili@users.noreply.github.com>
2023-10-23 22:41:49 -0400

    "changes on Resource and Location classes"

--- src/entity/Resource.java
+++ src/entity/Resource.java
@@ -1,4 +1,24 @@
 package entity;
 
+import java.io.File;
+
 public class Resource {
+    private String link;
+    private File bibTex;
+
+    // setters
+    public void setLink(String link){
+        this.link = link;
+    }
+    public void setBibtex(File bibtex){
+        this.bibTex = bibtex;
+    }
+
+    //getters
+    public File getBibtex(){
+        return this.bibTex;
+    }
+    public String getLink(){
+        return this.link;
+    }
 }
