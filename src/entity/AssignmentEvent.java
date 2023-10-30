7207de21ae2292304a3a5086366e770b05ae2bb1
Mikhovs <mikayel.hovsep@gmail.com>
2023-10-30 11:17:05 -0400

    refactored to extract helper method

--- src/entity/AssignmentEvent.java
+++ src/entity/AssignmentEvent.java
@@ -1,4 +1,45 @@
 package entity;
 
+import java.time.*;
+
 public class AssignmentEvent extends Event{
+
+    private String type;
+    private int percentage;
+    private boolean required;
+    private LocalDateTime lateDueDate;
+    private double[] percentageReduction;
+
+
+    public String getType(){
+        return "";
+    }
+    public void setType(String type){
+
+    }
+    public int getPercentage(){
+        return 0;
+    }
+    public void setPercentage(int percentage){
+
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+    public void setRequired(boolean req){
+
+    }
+    public LocalDateTime getLateDueDate(){
+        return lateDueDate;
+    }
+    public void setLateDueDate(LocalDateTime time){
+
+    }
+    public double getPercentageReduction(){
+        return percentageReduction[0];
+    }
+    public void setPercentageReduction(double[] list){
+
+    }
 }
--- src/entity/ClassEvent.java
+++ src/entity/ClassEvent.java
@@ -1,4 +1,13 @@
 package entity;
 
 public class ClassEvent extends Event{
+
+    String type;
+
+    public String getType(){
+        return type;
+    }
+    public void setType(){
+
+    }
 }
