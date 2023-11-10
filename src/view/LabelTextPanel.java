ee1b7c2993d17bd7662d5a56670d0e746ff41235
Maksim Tsoi <maksim.tsoi@mail.utoronto.ca>
2023-11-10 02:07:27 -0500

    Created a class for input fields.

---
+++ src/view/LabelTextPanel.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class LabelTextPanel extends JPanel {
+    LabelTextPanel(JLabel label, JTextField textField){
+        this.add(label);
+        this.add(textField);
+    }
+}
