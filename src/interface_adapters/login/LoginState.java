342599b30a4bd5170062a3af26ac8a3a0b4cb08a
Maksim Tsoi <maksim.tsoi@mail.utoronto.ca>
2023-11-10 00:30:45 -0500

    Implemented base functions for LoginState.

--- src/interface_adapters/login/LoginState.java
+++ src/interface_adapters/login/LoginState.java
@@ -1,5 +1,33 @@
 package interface_adapters.login;
 
 public class LoginState {
-    //TODO
+    private String email = "";
+    private String emailError = null;
+    private String password = "";
+    private String passwordError = null;
+
+    public LoginState(LoginState copy) {
+        email = copy.email;
+        emailError = copy.emailError;
+        password = copy.password;
+        passwordError = copy.passwordError;
+    }
+
+    public LoginState(){}
+
+    public String getEmail(){return email;}
+
+    public String getEmailError(){return emailError;}
+
+    public String getPassword(){return password;}
+
+    public String getPasswordError(){return passwordError;}
+
+    public void setEmail(String email){this.email = email;}
+
+    public void setEmailError(String emailError){this.emailError = emailError;}
+
+    public void setPassword(String password){this.password = password;}
+
+    public void setPasswordError(String passwordError){this.passwordError = passwordError;}
 }
