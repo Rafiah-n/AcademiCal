ce1ad6afb71123ad865f439c4ed354c926ea4b18
Maksim Tsoi <maksim.tsoi@mail.utoronto.ca>
2023-11-12 18:24:59 -0500

    Implemented User class in CommonUser.

---
+++ src/entity/CommonUser.java
@@ -0,0 +1,30 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+public class CommonUser implements User{
+
+    private final String email;
+    private final String password;
+    private final LocalDateTime creationTime;
+
+    /**
+     * Requires: password is valid.
+     * @param email
+     * param password
+     */
+    CommonUser(String email, String password, LocalDateTime creationTime){
+        this.email = email;
+        this.password = password;
+        this.creationTime =  creationTime;
+    }
+
+    @Override
+    public String getEmail(){return email;}
+
+    @Override
+    public String getPassword(){return password;}
+
+    @Override
+    public LocalDateTime getCreationTime(){return creationTime;}
+}
