5af12d3f6a36516cd4e35e97200bc2391e19237b
Maksim Tsoi <maksim.tsoi@mail.utoronto.ca>
2023-11-13 12:26:39 -0500

    Implemented execute function.

--- src/use_case/login/LoginInteractor.java
+++ src/use_case/login/LoginInteractor.java
@@ -1,5 +1,33 @@
-package use_case.logInUser;
+package use_case.login;
 
-public class loginInteractor {
-    //TODO
+import entity.User;
+
+public class LoginInteractor implements LoginInputBoundary{
+    final LoginUserDataAccessInterface userDataAccessObject;
+    final LoginOutputBoundary loginPresenter;
+
+    public LoginInteractor(LoginUserDataAccessInterface userDataAccessInterface,
+                           LoginOutputBoundary loginOutputBoundary){
+        this.userDataAccessObject = userDataAccessInterface;
+        this.loginPresenter = loginOutputBoundary;
+    }
+
+    @Override
+    public void execute(LoginInputData loginInputData) {
+        String email = loginInputData.getEmail();
+        String password = loginInputData.getPassword();
+        if (!userDataAccessObject.existsByEmail(email)) {
+            loginPresenter.prepareFailView(email + ":Email does not exist");
+        } else {
+            String pwd = userDataAccessObject.get(loginInputData.getEmail()).getPassword();
+            if (!password.equals(pwd)) {
+                loginPresenter.prepareFailView("Incorrect Password for" + email + ".");
+            } else {
+                User user = userDataAccessObject.get(loginInputData.getEmail());
+
+                LoginOutputData loginOutputData = new LoginOutputData(user.getEmail(), false);
+                loginPresenter.prepareSuccessView(loginOutputData);
+            }
+        }
+    }
 }
