cd44a71f18bc2f07b91ea89023c565ead3f6cf0b
ksaykili <106082029+ksaykili@users.noreply.github.com>
2023-11-23 17:24:07 -0500

    "After modifying the implementation of UpdateEventState, changed a line on the file

---
+++ src/interface_adapters/UpdateEventViewModel.java
@@ -0,0 +1,54 @@
+package interface_adapters;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class UpdateEventViewModel extends ViewModel {
+
+    public final String TITLE_LABEL = "Update Event View";
+    public final String EVENT_NAME_LABEL = "Event Name";
+    public final String COURSE_LABEL = "Course";
+    public final String EVENT_COMPLETED_LABEL = "Event Completed";
+    public final String ASSIGNMENT_TYPE_LABEL = "Assignment Type";
+    public final String ASSIGNMENT_PERCENTAGE_LABEL = "Assignment Percentage";
+    public final String ASSIGNMENT_REQUIRED_LABEL = "Assignment Required";
+    public final String RESOURCE_LABEL = "Resource";
+    public final String START_TIME_LABEL = "Start Time";
+    public final String END_TIME_LABEL = "End Time";
+    public final String LATE_DUE_DATE_LABEL = "Late Due Date";
+    public final String LOCATION_LABEL = "Location";
+    public final String PERCENTAGE_REDUCTION_LABEL = "Percentage Reduction";
+    public final String CLASS_TYPE_LABEL = "Class Type";
+    public final String READING_PAGES_LABEL = "Reading Pages";
+    public final String STUDY_TODO_LABEL = "Study To-Do";
+
+    public final String UPDATE_BUTTON_LABEL = "Update";
+    public final String CANCEL_BUTTON_LABEL = "Cancel";
+
+    private UpdateEventState state = new UpdateEventState();
+
+    public UpdateEventViewModel() {
+        super("update event");
+    }
+
+    public void setState(UpdateEventState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the UpdateEvent Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public UpdateEventState getState() {
+        return state;
+    }
+}
+
