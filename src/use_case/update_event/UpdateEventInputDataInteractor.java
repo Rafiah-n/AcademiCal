735de9d30f1f9fb55927a2278bc9c7f41bc0baa5
ksaykili <106082029+ksaykili@users.noreply.github.com>
2023-11-25 16:06:31 -0500

    Added a method to check uniqueness of attributes

--- src/use_case/update_event/UpdateEventInputDataInteractor.java
+++ src/use_case/update_event/UpdateEventInputDataInteractor.java
@@ -77,4 +77,70 @@ public class UpdateEventInputDataInteractor {
     public static void updateStudyTodoList(StudyEvent studyEvent, List<String> newStudyTodo) {
         studyEvent.addTodo(newStudyTodo.toString());
     }
+
+
+
+    // here are the methods for checking whether the current value of an entity is same as the value we
+    // want to update to.
+
+    public static boolean isSameValue(Event event, String newName) {
+        return (newName.equals(event.getName()));
+    }
+
+    public static boolean isSameValue(Event event, Course newCourse) {
+        return (newCourse.equals(event.getCourse()));
+    }
+
+    public static boolean isSameValue(Event event, LocalDateTime newStartTime) {
+        return (newStartTime.equals(event.getStartTime()));
+    }
+
+    public static boolean isSameValue(Event event, LocalDateTime newEndTime) {
+        return (newEndTime.equals(event.getEndTime()));
+    }
+
+    public static boolean isSameValue(Event event, Location newLocation) {
+        return (newLocation.equals(event.getLocation()));
+    }
+
+    public static boolean isSameValue(AssignmentEvent assignmentEvent, String newAssignmentType) {
+        return (newAssignmentType.equals(assignmentEvent.getType()));
+    }
+
+    public static boolean isSameValue(AssignmentEvent assignmentEvent, int newAssignmentPercentage) {
+        return (newAssignmentPercentage == assignmentEvent.getPercentage());
+    }
+
+    public static boolean isSameValue(AssignmentEvent assignmentEvent, boolean newAssignmentRequired) {
+        return (newAssignmentRequired == assignmentEvent.isRequired());
+    }
+
+    public static boolean isSameValue(AssignmentEvent assignmentEvent, LocalDateTime newAssignmentLateDueDate) {
+        return (newAssignmentLateDueDate.equals(assignmentEvent.getLateDueDate()));
+    }
+
+    public static boolean isSameValue(AssignmentEvent assignmentEvent, List<Double> newAssignmentPercentageReduction) {
+        return (newAssignmentPercentageReduction.equals(assignmentEvent.getPercentageReduction()));
+    }
+
+    public static boolean isSameValue(ReadingEvent readingEvent, Resource newReadingResource) {
+        return (newReadingResource.equals(readingEvent.getResource()));
+    }
+
+    public static boolean isSameValue(ReadingEvent readingEvent, boolean newReadingRequired) {
+        return (newReadingRequired == readingEvent.isRequired());
+    }
+
+    public static boolean isSameValue(ReadingEvent readingEvent, List<Integer> newReadingPages) {
+        return (newReadingPages.equals(readingEvent.getPages()));
+    }
+
+    public static boolean isSameValue(StudyEvent studyEvent, List<String> newStudyTodo) {
+        return (newStudyTodo.equals(studyEvent.getTodo()));
+    }
+
+
+    public static void execute(UpdateEventInputData updateEventInputData) {
+
+    }
 }
