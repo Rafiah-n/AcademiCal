150bf6737d4578eb9d6de5d3e7be4597c944e3da
Mikhovs <mikayel.hovsep@gmail.com>
2023-12-02 20:02:18 -0500

    refactored to extract helper method

--- src/main/java/use_case/deleteEvent.java
+++ src/main/java/use_case/deleteEvent.java
@@ -5,12 +5,14 @@ import com.google.api.services.calendar.Calendar;
 public class deleteEvent() {
 
     final private Event event;
+    Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credentials)
+            .setApplicationName("applicationName").build();
 
     public deleteEvent(Event event){
         this.event = event;
     }
-    Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credentials)
-            .setApplicationName("applicationName").build();
 
-    service.events().delete("primary", event.getId()).execute();
+    public void delete(){
+        service.events().delete("primary", event.getId()).execute();
+    }
 }
