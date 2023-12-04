11aba8aa4704a0961948192f0875a8c520561748
Rafiah-n <54577192+Rafiah-n@users.noreply.github.com>
2023-12-03 19:40:10 -0500

    added Event Facotry and CommonEventFactory

---
+++ src/main/java/entity/EventFactory.java
@@ -0,0 +1,8 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+public interface EventFactory {
+    Event create(String name, LocalDateTime startTime, LocalDateTime endTime, String location,
+                        boolean completed, String eventId);
+}
